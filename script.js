c265faa021335cf3811f5d6d150e78e39e5c024b
TURX-Site <hello@turrx.com>
2025-09-30 00:33:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form submission alert
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  this.reset();
+});
